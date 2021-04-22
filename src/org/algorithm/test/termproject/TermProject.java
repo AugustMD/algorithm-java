2624905b43eb0ec9f4a44ed054b1e7fc9d762c2c
AugustMD <sungjin.park0129@gmail.com>
2021-04-22 23:33:37 +0900

    dfs 수정

--- src/org/algorithm/test/termproject/TermProject.java
+++ src/org/algorithm/test/termproject/TermProject.java
@@ -1,5 +1,43 @@
 package org.algorithm.test.termproject;
 
-public class TermProject {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TermProject {
+	private static boolean[]  visits;
+	private static List<Integer>  students;
+	private static List<Integer>  finals;
+	private static int count;
+	public int onlyOne(int length, List<Integer> inputs) {
+		int result = 0;
+		int count = 0;
+		students = new ArrayList<>();
+		
+		students.addAll(inputs);
+		
+		for(int i=0; i<length; i++) {
+			visits = new boolean[length];
+			dfs(i);
+//			for(boolean b : visits) {
+//				System.out.print(b + " ");
+//			}
+			System.out.println();
+		}
+		
+		return result;
+	}
+	
+	private void dfs(int x) {
+		if(visits[x]) return;
+		
+		visits[x] = true;
+		
+		System.out.print(x + " ");
+		
+		for(int student:students) {
+			if(visits[student-1]) {
+				dfs(student-1);
+			}
+		}
+	}
 }
--- src/org/algorithm/test/termproject/TermProjectMain.java
+++ src/org/algorithm/test/termproject/TermProjectMain.java
@@ -1,10 +1,44 @@
 package org.algorithm.test.termproject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TermProjectMain {
 	
 	// 테스트 실행 메인 클래스
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		TermProject termProject = new TermProject();
+		
+		/**
+		 * 테스트 케이스
+		 */
+		int t = scanner.nextInt();
+		
+		int[] output = new int[t];
+		
+		for(int i=0; i<t; i++) {
+			/**
+			 * 학생 수
+			 */
+			int n = scanner.nextInt();
+			
+			/**
+			 * 선택한 학생
+			 */
+			List<Integer> students = new ArrayList<>();
+			
+			for(int j=0; j<n; j++) {
+				students.add(scanner.nextInt());
+			}
+			
+			output[i] = termProject.onlyOne(n, students);
+		}
+		
+		for(int i=0; i<t; i++) {
+			System.out.println(output[i]);
+		}
 
 	}
 
